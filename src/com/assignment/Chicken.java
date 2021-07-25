67006968945b9968dc42b24e03ace4dfc669ef43
yogaNaseem <87707947+yogaNaseem@users.noreply.github.com>
2021-07-25 18:47:42 +0530

    BONUS - Sound in native Language
    Can you add a second language (if you kno w a language other than English) Use the
    rooster as a PoC for demonstrating this. For example, this is how the Rooster sounds
    differently depending on language. Please add the rooster sound in your native tongue.
    o Danish: kykyliky
    o Dutch: kukeleku
    o Finnish: kukko kiekuu
    o French: cocorico
    o German: kikeriki
    o Greek: kikiriki
    o Hebrew: coo- koo - ri-koo
    o Hungarian: kukuriku
    o Italian: chicchirichi
    o Japanese: ko - ke - kok - ko -o
    o Portuguese: cucurucu
    o Russian: kukareku
    o Swedish: kuckeliku
    o Turkish: kuk-kurri-kuuu
    o Urdu: kuklooku

--- src/com/assignment/Chicken.java
+++ src/com/assignment/Chicken.java
@@ -1,12 +1,27 @@
 package com.assignment;
 
+import java.util.HashMap;
+
 public class Chicken extends Bird {
-	
+
+	public static HashMap<String, String> languageTranslator = new HashMap<String, String>();
+
+	public void setLanguageTranslator(HashMap<String, String> t) {
+		Chicken.languageTranslator = t;
+	}
+
 	@Override
-	public void sing(){
-		if(this.getSex() == Sex.FEMALE)
+	public void sing() {
+		if (this.getSex() == Sex.FEMALE)
 			System.out.println("Cluck, cluck");
 		else
 			System.out.println("Cock-a-doodle-doo");
+	}
+	
+	public void singInNativeTongue(String Language){
+		if(this.getSex() == Sex.FEMALE)
+			System.out.println("Cluck, cluck");
+		else
+			System.out.println(languageTranslator.get(Language));
 	}
 }
--- src/com/assignment/Solution.java
+++ src/com/assignment/Solution.java
@@ -1,6 +1,7 @@
 package com.assignment;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import com.assignment.Animal.Sex;
@@ -11,67 +12,100 @@ public class Solution {
 	static List<Animal> animals;
 	public static void main(String[] args) {
 		
-		Bird bird = new Bird();
-		System.out.println("Bird");
-		bird.walk();
-	//	bird.fly();
-		bird.sing();
+		printA1Answer();
 
-		Duck duck = new Duck();
-		System.out.println("Duck");
-		duck.sing();
-		duck.swim();
+		printA2Answer();
 		
-		Chicken chicken = new Chicken();
-		System.out.println("Chicken");
-		chicken.sing();
+		printA3Answer();
 		
-		Chicken chicken1 = new Chicken();
-		chicken1.setSex(Sex.FEMALE);
-		System.out.println("Chicken");
-		chicken1.sing();
-		Chicken rooster = new Chicken();
-		rooster.setSex(Sex.MALE);
-		System.out.println("Rooster");
-		rooster.sing();
-		
-		ParrotLivingWithDog parrotLivingWithDog = new ParrotLivingWithDog(new Bird());
-		System.out.println("parrotLivingWithDog");
-		parrotLivingWithDog.sing();
-		
-		ParrotLivingWithCat parrotLivingWithCat = new ParrotLivingWithCat(new Parrot());
-		System.out.println("parrotLivingWithCat");
-		parrotLivingWithCat.sing();
-		
-		ParrotLivingWithRooster parrotLivingWithRooster = new ParrotLivingWithRooster(new Bird());
-		System.out.println("parrotLivingWithRooster");
-		parrotLivingWithRooster.sing();
+		printA4Answer();
 		
 		
-		Fish fish = new Fish();
-		System.out.println("Fish");
-		fish.swim();
+		printB1Answer();
 		
-		ClownFish clownFish = new ClownFish(Size.SMALL, Colour.ORANGE);
-		System.out.println("clownFish");
-		clownFish.makeJokes();
+		printB2Answer();
 		
-		SharkFish sharkFish = new SharkFish(Size.LARGE, Colour.GREY);
-		System.out.println("sharkFish");
-		sharkFish.eat(clownFish);
+		printB3Answer();
 		
-		ButterFly b = new ButterFly();
-		System.out.println("ButterFly");
-		b.fly();
+		printC1Answer();
 		
-		Catterpillar c = new Catterpillar();
-		System.out.println("Catterpillar");
-		c.walk();
+		printC2Answer();
 		
-		ButterFly butterFlyFromCaterpillar = new Metamorphosis().trnasform(c);
-		System.out.println("butterFlyFromCaterpillar");
-		butterFlyFromCaterpillar.fly();
+		printDAnswer();
 		
+		
+		printBonus1Answer();
+		
+		
+	}
+	private static void printBonus1Answer() {
+		Chicken secondLangC = new Chicken();
+		secondLangC.setSex(Sex.MALE);
+		
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("Danish", "kykyliky");
+		map.put("Dutch", "kukeleku");
+		map.put("Finnish", "kukko kiekuu");
+		map.put("French", "cocorico");
+		map.put("German", "kikeriki");
+		map.put("Greek", "kikiriki");
+		map.put("Hebrew", "coo- koo - ri-koo");
+		map.put("Hungarian", "kukuriku");
+		map.put("Italian", "chicchirichi");
+		map.put("Japanese", "ko - ke - kok - ko -o");
+		map.put("Portuguese", "cucurucu");
+		map.put("Russian", "kukareku");
+		map.put("Swedish", "kuckeliku");
+		map.put("Turkish", "kuk-kurri-kuuu");
+		map.put("Urdu", "kuklooku");
+		Chicken.languageTranslator = map;
+		
+		System.out.println("Native language is Danish");
+		secondLangC.singInNativeTongue("Danish");
+		
+		System.out.println("Native language is Dutch");
+		secondLangC.singInNativeTongue("Dutch");
+		
+		System.out.println("Native language is Finnish");
+		secondLangC.singInNativeTongue("Finnish");
+		
+		System.out.println("Native language is French");
+		secondLangC.singInNativeTongue("French");
+		
+		System.out.println("Native language is German");
+		secondLangC.singInNativeTongue("German");
+		
+		System.out.println("Native language is Greek");
+		secondLangC.singInNativeTongue("Greek");
+		
+		System.out.println("Native language is Hebrew");
+		secondLangC.singInNativeTongue("Hebrew");
+		
+		System.out.println("Native language is Hungarian");
+		secondLangC.singInNativeTongue("Hungarian");
+		
+		System.out.println("Native language is Italian");
+		secondLangC.singInNativeTongue("Italian");
+		
+		System.out.println("Native language is Japanese");
+		secondLangC.singInNativeTongue("Japanese");
+		
+		System.out.println("Native language is Portuguese");
+		secondLangC.singInNativeTongue("Portuguese");
+		
+		System.out.println("Native language is Russian");
+		secondLangC.singInNativeTongue("Russian");
+		
+		System.out.println("Native language is Swedish");
+		secondLangC.singInNativeTongue("Swedish");
+		
+		System.out.println("Native language is Turkish");
+		secondLangC.singInNativeTongue("Turkish");
+		
+		System.out.println("Native language is Urdu");
+		secondLangC.singInNativeTongue("Urdu");
+	}
+	private static void printDAnswer() {
 		animals = new ArrayList<Animal>();
 		animals.add(AnimalFactory.getAnimal("BIRD"));
 		animals.add(AnimalFactory.getAnimal("DUCK"));
@@ -104,6 +138,79 @@ public class Solution {
 		System.out.println("No of animals that can  sing is " + sing);
 		System.out.println("No of animals that can  swim is " + swim);
 		System.out.println("No of animals that can  walk is " + walk);
+	}
+	private static void printC2Answer() {
+		Catterpillar c = new Catterpillar();
+		System.out.println("Catterpillar");
+		c.walk();
+		
+		ButterFly butterFlyFromCaterpillar = new Metamorphosis().trnasform(c);
+		System.out.println("butterFlyFromCaterpillar");
+		butterFlyFromCaterpillar.fly();
+	}
+	private static void printC1Answer() {
+		ButterFly b = new ButterFly();
+		System.out.println("ButterFly");
+		b.fly();
+	}
+	private static void printB3Answer() {
+		Dolphins dolphins = new Dolphins();
+		System.out.println("Dolphins");
+		dolphins.swim();
+	}
+	private static void printB2Answer() {
+		ClownFish clownFish = new ClownFish(Size.SMALL, Colour.ORANGE);
+		System.out.println("clownFish");
+		clownFish.makeJokes();
+		
+		SharkFish sharkFish = new SharkFish(Size.LARGE, Colour.GREY);
+		System.out.println("sharkFish");
+		sharkFish.eat(clownFish);
+	}
+	private static void printB1Answer() {
+		Fish fish = new Fish();
+		System.out.println("Fish");
+		fish.swim();
+	}
+	private static void printA4Answer() {
+		ParrotLivingWithDog parrotLivingWithDog = new ParrotLivingWithDog(new Bird());
+		System.out.println("parrotLivingWithDog");
+		parrotLivingWithDog.sing();
+		
+		ParrotLivingWithCat parrotLivingWithCat = new ParrotLivingWithCat(new Parrot());
+		System.out.println("parrotLivingWithCat");
+		parrotLivingWithCat.sing();
+		
+		ParrotLivingWithRooster parrotLivingWithRooster = new ParrotLivingWithRooster(new Bird());
+		System.out.println("parrotLivingWithRooster");
+		parrotLivingWithRooster.sing();
+	}
+	private static void printA3Answer() {
+		Chicken chicken1 = new Chicken();
+		chicken1.setSex(Sex.FEMALE);
+		System.out.println("Chicken");
+		chicken1.sing();
+		Chicken rooster = new Chicken();
+		rooster.setSex(Sex.MALE);
+		System.out.println("Rooster");
+		rooster.sing();
+	}
+	private static void printA2Answer() {
+		Duck duck = new Duck();
+		System.out.println("Duck");
+		duck.sing();
+		duck.swim();
+		
+		Chicken chicken = new Chicken();
+		System.out.println("Chicken");
+		chicken.sing();
+	}
+	private static void printA1Answer() {
+		Bird bird = new Bird();
+		System.out.println("Bird");
+		bird.walk();
+	//	bird.fly();
+		bird.sing();
 	}
 
 }
